7c9006bd5a957034dbf1e6443fa0f4c962de2d09
VforTalent <131975164+VforTalent@users.noreply.github.com>
2025-04-19 15:43:15 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,3 @@
-
 export default async function handler(req, res) {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.status(400).send("Missing url");
@@ -7,7 +6,7 @@ export default async function handler(req, res) {
     const response = await fetch(targetUrl);
     let html = await response.text();
 
-    // Supprimer les entêtes HTML gênants
+    // Supprimer les entêtes bloquantes
     html = html.replace(/<meta[^>]+http-equiv=["']?X-Frame-Options["']?[^>]*>/gi, '');
     html = html.replace(/content-security-policy[^>]*>/gi, '');
 
